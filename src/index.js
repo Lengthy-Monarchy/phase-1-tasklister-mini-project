831f9a151667928f590a2b1863aa6457a092d23e
anne <anneirungu07@gmail.com>
2024-01-11 22:00:39 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const tasksList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    const newTask = document.createElement('li');
+    newTask.textContent = taskDescription;
+
+    tasksList.appendChild(newTask);
+
+    form.reset();
+  });
 });
